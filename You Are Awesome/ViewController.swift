67e6c21d559ac78840fe75038eecff0ff4534d32
Maxwell Rodriguez <maxwell.e.rodriguez@gmail.com>
2022-01-25 16:50:44 -0800

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Max on 1/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ You Are AwesomeUITests/You_Are_AwesomeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  You_Are_AwesomeUITestsLaunchTests.swift
+//  You Are AwesomeUITests
+//
+//  Created by Max on 1/25/22.
+//
+
+import XCTest
+
+class You_Are_AwesomeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
